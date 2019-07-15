8f110174fc4f6ad72dd5d3e07be5ec6d0b98bdbf
Gabriel Sanzone <gabriel.sanzone@fcamara.com.br>
2019-07-15 11:31:47 -0300

    0.0.0.0
    Criação doprojeto

---
+++ Quero Conhecer/Quero Conhecer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quero Conhecer
+//
+//  Created by Gabriel Chirico Mahtuk de Melo Sanzone on 15/07/19.
+//  Copyright © 2019 Gabriel Chirico Mahtuk de Melo Sanzone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
